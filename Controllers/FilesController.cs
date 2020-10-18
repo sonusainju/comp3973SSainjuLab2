5d80de483b8d8f86edeb8697a4c4de2848b6ec9a
sonusainju <sainjusonu@gmail.com>
2020-10-18 17:23:27 -0600

    first commit

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Aspcore.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Aspcore.Controllers
+{
+    public class FilesController : Controller
+    {
+        private readonly ILogger<FilesController> _logger;
+
+        private IWebHostEnvironment Environment;
+
+        public FilesController(IWebHostEnvironment _environment)
+            {
+                Environment = _environment;
+            }
+        public IActionResult Index()
+        {
+            
+            string contentPath = this.Environment.ContentRootPath;
+            string[] files = Directory.GetFiles(contentPath + @"\TextFiles");
+
+            return View(files);
+        }  
+        public IActionResult content(int id)
+        {     
+            string contentPath = this.Environment.ContentRootPath;
+            string[] files = Directory.GetFiles(contentPath + @"\TextFiles");
+            
+            string myString  ="Empty File";  
+            string myFile = files[id].ToString();
+
+            using (StreamReader sr = new StreamReader(myFile))
+            {
+                myString = sr.ReadToEnd();
+            }
+
+            ViewData["Content"] = myString;
+
+            return View();
+        } 
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
